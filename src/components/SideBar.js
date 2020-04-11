445f0992c833fe100ac58a243486c0a3955e5f31
DanielMitiku <wedanielmitiku@gmail.com>
2020-04-11 23:54:53 +0300

    add layout styles for components

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import daniel_wesego_img from '../img/daniel_wesego.jpg';
-import './sidebar.css';
 
 
 function SideBar() {
 
   return (
-    <div>
-      <div>
-        <h1>Hello from SideBar</h1>
-      </div>
+    <div className="sideDiv">
+      <h1>Hello from SideBar</h1>
     </div>
   );
 }

7cd2e69b406099ce22d500303b237de98d574b32
DanielMitiku <wedanielmitiku@gmail.com>
2020-04-12 16:48:34 +0300

    add contact form and emailJS

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,12 +1,80 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
 
 
-function ContactForm() {
-  return (
-    <div>
-      <h1>Hello from ContactForm</h1>
-    </div>
-  );
+class ContactForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sent: false,
+      user_name: '',
+      user_email: '',
+      message: '',
+      loading: false,
+    }
+    this.sendEmail = this.sendEmail.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  sendEmail = (e) => {
+    e.preventDefault();
+    this.setState({...this, loading: true});
+
+    emailjs.sendForm('gmail', 'template_Yipl3e7D', e.target, 'user_jrwB7E17ognPgubGmS5PW')
+      .then(() => {
+          this.setState(
+            {...this,
+            sent: true,
+            user_name: '',
+            user_email: '',
+            message: '',
+            loading: false});
+      }, (error) => {
+          console.log(error.text);
+          this.setState({...this, loading: false});
+      });
+  }
+
+  render() {
+    const { sent, user_email, user_name, message, loading } = this.state;
+    const load = (<div className="d-flex justify-content-center">
+                      <div className="spinner-border text-primary" role="status">
+                       <span className="sr-only">Loading...</span>
+                      </div>
+                    </div>);
+    const thankyou = <p style={{color: 'purple'}}>Thank you for contacting me! I will be in touch</p>;
+    return (
+      <div className="my-3">
+        <h2>Contact Me</h2>
+        { sent && thankyou }
+        { loading && load}
+        <div className="col-md-6 col-8 offset-2 offset-md-3">
+        <form onSubmit={this.sendEmail}>
+          <input type="hidden" name="contact_number" />
+          <div className="form-group text-left my-3">
+            <label htmlFor="name">Your Name</label>
+            <input type="text" className="form-control" id="name" name="user_name" placeholder="Enter Name" value={user_name} onChange={this.handleChange} />
+          </div>
+          <div className="form-group text-left my-3">
+            <label htmlFor="email">Email address</label>
+            <input type="email" className="form-control" id="email" name="user_email" placeholder="Enter email" value={user_email} onChange={this.handleChange} />
+          </div>
+          <div className="form-group text-left my-3">
+            <label htmlFor="message">Your Message</label>
+            <textarea className="form-control" id="textarea" rows="4" name="message" value={message} onChange={this.handleChange}></textarea>
+          </div>
+          <button type="submit" className={`btn btn-primary mb-3 ${loading ? "disabled" : ""}`}>Send</button>
+        </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ContactForm;
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -10,15 +10,15 @@ function SideBar() {
       <div className="sideDiv my-5">
         <h3>Hi, I am </h3>
         <h1>Daniel Wesego</h1>
-        <div class="col-md-8 col-6 my-3 offset-md-2 offset-3 text-center">
-          <img src={daniel_wesego_img} class="img-fluid mx-auto d-block rounded-circle" alt="Daniel Wesego"></img>
-          <h3 class="mt-4">Full Stack Developer</h3>
+        <div className="col-md-8 col-6 my-3 offset-md-2 offset-3 text-center">
+          <img src={daniel_wesego_img} className="img-fluid mx-auto d-block rounded-circle" alt="Daniel Wesego"></img>
+          <h3 className="mt-4">Full Stack Developer</h3>
           <p>Ruby on Rails | React | Redux</p>
           <br />
-          <a target="_blank" rel="noopener noreferrer" href="mailto: wedanielmitiku@gmail.com"><i class="fas fa-envelope fa-2x mx-2"></i></a>
-          <a target="_blank" rel="noopener noreferrer" href="https://github.com/DanielMitiku"><i class="fab fa-github fa-2x mx-2"></i></a>
-          <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/daniel-wesego/"><i class="fab fa-linkedin fa-2x mx-2"></i></a>
-          <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/daniel_wesego"><i class="fab fa-twitter fa-2x mx-2"></i></a>
+          <a target="_blank" rel="noopener noreferrer" href="mailto: wedanielmitiku@gmail.com"><i className="fas fa-envelope fa-2x mx-2"></i></a>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/DanielMitiku"><i className="fab fa-github fa-2x mx-2"></i></a>
+          <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/daniel-wesego/"><i className="fab fa-linkedin fa-2x mx-2"></i></a>
+          <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/daniel_wesego"><i className="fab fa-twitter fa-2x mx-2"></i></a>
         </div>
       </div>
       <ParticlesBg type="cobweb" bg={true} />

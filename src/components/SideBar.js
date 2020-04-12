4330ee6e452c267232c56e750fbfa064a46ce6e5
DanielMitiku <wedanielmitiku@gmail.com>
2020-04-12 14:13:51 +0300

    update sidebar with image

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -5,8 +5,14 @@ import daniel_wesego_img from '../img/daniel_wesego.jpg';
 function SideBar() {
 
   return (
-    <div className="sideDiv">
-      <h1>Hello from SideBar</h1>
+    <div className="sideDiv my-5">
+      <h3>Hi, I am </h3>
+      <h1>Daniel Wesego</h1>
+      <div class="col-md-8 col-6 my-3 offset-md-2 offset-3 text-center">
+        <img src={daniel_wesego_img} class="img-fluid mx-auto d-block rounded-circle" alt="Daniel Wesego"></img>
+        <h3 class="mt-4">Full Stack Developer</h3>
+        <p>Ruby on Rails | React | Redux</p>
+      </div>
     </div>
   );
 }

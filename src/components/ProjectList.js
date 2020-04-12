33c5d218e3aba9ce50cb2aedd54aab21d17c2720
DanielMitiku <wedanielmitiku@gmail.com>
2020-04-13 00:28:43 +0300

    update project list

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import Project from './Project';
+import Projects from './Projects';
+import book_a_session_frontend from '../img/book_a_session.png';
+import book_a_session_backend from '../img/book_a_session_backend.png';
 
 
 function ProjectList() {
+  const list = {
+    projects: [
+      {
+        title: "Book a Session Frontend",
+        githubLink: "https://github.com/DanielMitiku/book-a-counseling-session-frontend",
+        liveLink: "",
+        image: book_a_session_frontend,
+      },
+      {
+        title: "Book a Session Backend",
+        githubLink: "https://github.com/DanielMitiku/book-a-counseling-session-backend",
+        liveLink: "",
+        image: book_a_session_backend,
+      },
+    ],
+  }
+
   return (
     <div>
-      <h1>Hello from ProjectList</h1>
-      <Project />
+      <h2 className="my-4">Here are some of my Projects</h2>
+      <i className="fas fa-arrow-down fa-4x mb-4"></i>
+      <Projects list={list}/>
     </div>
   );
 }

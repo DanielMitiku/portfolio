9181dc3ed5a2de1cbc2d5c706b832cac8309dff8
DanielMitiku <wedanielmitiku@gmail.com>
2020-04-13 21:54:35 +0300

    add hover text to the Project images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,21 +4,27 @@ import React from 'react';
 function Projects({list}) {
   return (
     <div>
-      <div className="col-10 offset-1">
+      <div className="col-sm-10 offset-sm-1 col-12">
         { list.projects.map(project => {
           return (
             <div key={project.title} className="">
               <h3 className="mb-4">{project.title}</h3>
-              <img className="img-fluid" src={project.image} alt={project.title} />
+              <div className="card bg-dark text-white project-image-div">
+                <img className="card-img project-image" src={project.image} alt={project.title} />
+                <div className="card-img-overlay project-image-texts">
+                  <h5 className="card-title project-image-text">{project.header}</h5>
+                  <h5 className="card-title project-image-text">{project.description}</h5>
+                </div>
+              </div>
               <div className="row my-3">
                 <div className="col-md-6 source-code">
-                  <a target="_blank" rel="noopener noreferrer" href={project.githubLink} className="my-1 btn btn-outline-dark" role="button" aria-pressed="true"><i className="fas fa-code "></i> Source Code</a>
+                  <a target="_blank" rel="noopener noreferrer" href={project.githubLink} className="my-1 btn btn-outline-dark" role="button" aria-pressed="true"><i className="fas fa-code"></i> Source Code</a>
                 </div>
                 <div className="col-md-6 live-link">
                   <a target="_blank" rel="noopener noreferrer" href={project.liveLink} className="my-1 btn btn-outline-dark" role="button" aria-pressed="true"><i className="fas fa-cloud"></i> Live Demo</a>
                 </div>
               </div>
-              </div>
+            </div>
           );
         }
         )}

9fb100d5735c716b0f90b79afa21bd6f52de6f78
DanielMitiku <wedanielmitiku@gmail.com>
2020-04-13 23:25:53 +0300

    add about component

--- src/components/MainBar.js
+++ src/components/MainBar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ProjectList from './ProjectList';
 import ContactForm from './ContactForm';
+import About from './About';
 
 
 function MainBar() {
   return (
     <div>
       <ProjectList />
+      <About />
       <ContactForm />
     </div>
   );

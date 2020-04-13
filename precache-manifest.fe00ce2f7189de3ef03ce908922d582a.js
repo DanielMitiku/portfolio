6df120ff61017437505cdc2b6d7c2b1cf560f938
DanielMitiku <wedanielmitiku@gmail.com>
2020-04-13 23:53:49 +0300

    Updates

---
+++ precache-manifest.fe00ce2f7189de3ef03ce908922d582a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4770120dd2e2b10c88dac52b90b6300e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2dd8b98a623952b4608d",
+    "url": "/portfolio/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "e774ccb1ce2c91e37355",
+    "url": "/portfolio/static/css/main.a40d9bbe.chunk.css"
+  },
+  {
+    "revision": "2dd8b98a623952b4608d",
+    "url": "/portfolio/static/js/2.2098ffa7.chunk.js"
+  },
+  {
+    "revision": "6dea14eab83053243865ab6f2019f727",
+    "url": "/portfolio/static/js/2.2098ffa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e774ccb1ce2c91e37355",
+    "url": "/portfolio/static/js/main.cad7a0b1.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "f0c4676d42a7f655d2e1bbc77ce8f6b4",
+    "url": "/portfolio/static/media/battleship.f0c4676d.png"
+  },
+  {
+    "revision": "f322c56e41104acbc4a5b0d0f646ce48",
+    "url": "/portfolio/static/media/book_a_session.f322c56e.png"
+  },
+  {
+    "revision": "eab81817a8c3c6f012d54dc0b0405b54",
+    "url": "/portfolio/static/media/book_a_session_backend.eab81817.png"
+  },
+  {
+    "revision": "b5d4bcb8d9f70caa5e4da9033f0c4b2f",
+    "url": "/portfolio/static/media/daniel.b5d4bcb8.jpg"
+  },
+  {
+    "revision": "518abbb3f4f26bdf8dbb17f56ac00ceb",
+    "url": "/portfolio/static/media/rails_book.518abbb3.png"
+  },
+  {
+    "revision": "00f06e08a639d086296c3a116017c122",
+    "url": "/portfolio/static/media/weather_app.00f06e08.png"
+  }
+]);
